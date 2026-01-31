c5bf42786333346fd9282f65e4ae6dbd185b040c
matilde <matildejimenezbedmar@gmail.com>
2026-01-31 19:15:18 +0100

    badges corregidos

--- ComfortFood_front/resources/views/livewire/pending-orders-badge.blade.php
+++ ComfortFood_front/resources/views/livewire/pending-orders-badge.blade.php
@@ -1,7 +1,7 @@
 <div wire:poll.10s>
     @if($count > 0)
         <span title="Pedidos pendientes"
-            class="ml-15 absolute inline-flex items-center justify-center px-3 py-0.5 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+            class="ml-10 -mt-4 absolute inline-flex items-center justify-center px-3 py-0.5 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
             {{ $count }}
         </span>
     @endif
