238e89b52e4984fbb650b1bd3273db15ff694b17
matilde <matildejimenezbedmar@gmail.com>
2026-01-31 18:35:31 +0100

    restaurante inactivo se ocultará a los clientes

--- ComfortFood_front/app/Livewire/Favorites.php
+++ ComfortFood_front/app/Livewire/Favorites.php
@@ -11,7 +11,8 @@ class Favorites extends Component
     public function toggleFavorite($menuId)
     {
         $cliente = auth()->user()->cliente;
-        if (!$cliente) return;
+        if (!$cliente)
+            return;
 
         $favorito = Favorito::where('id_cliente', $cliente->id_cliente)
             ->where('id_menu', $menuId)
@@ -30,6 +31,9 @@ class Favorites extends Component
         if ($cliente) {
             $favorites = Favorito::where('id_cliente', $cliente->id_cliente)
                 ->whereNotNull('id_menu')
+                ->whereHas('menu.restaurante.user', function ($query) {
+                    $query->where('es_activo', true);
+                })
                 ->with(['menu.restaurante.user', 'menu.favoritos'])
                 ->latest()
                 ->get()
