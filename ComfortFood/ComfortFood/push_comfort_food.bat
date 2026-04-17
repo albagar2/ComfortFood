7756c89148f44aacffade150718dffb6b5617254
albagar2 <baciapez@gmail.com>
2026-04-17 10:09:53 +0200

    Reorganize: Flatten repository to root

--- ComfortFood/ComfortFood/push_comfort_food.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd "C:\Users\bacia\Desktop\portfolio\proyectos\ProyectoFinal2DAW - copia\ComfortFood"
-git init
-git add .
-git commit -m "Initial commit for ComfortFood project from Portfolio"
-git branch -M main
-git remote add origin https://github.com/albagar2/ComfortFood.git
-git push -u origin main
-echo DONE
--- ComfortFood/resources/views/admin/users/index.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layouts::app title="Lista de usuarios">
-    <div class="p-6">
-        <livewire:admin.user-list />
-    </div>
-</x-layouts::app>
